84bf219b7ca660378206c563b600f58f2359d656
金子璐 <ll@LuLu-King-2.local>
2026-08-26 14:44:11 -0400

    Add project files

---
+++ src/main.py
@@ -0,0 +1,8 @@
+def welcome_message(name):
+    return f"{name}, welcome to the Data Engineering course."
+
+
+if __name__ == "__main__":
+    name = input("Enter your name: ")
+    print(welcome_message(name))
+    
